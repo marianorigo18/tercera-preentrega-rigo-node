9ae9dc1bb1d034aed907c1c7a9893b048be24812
marianorigo18 <marianowagnerrigo@gmail.com>
2023-05-21 20:36:37 -0300

    fix commit

--- src/clases/ProductsManager.js
+++ src/clases/ProductsManager.js
@@ -59,12 +59,73 @@ export default class ProductsManager{
         }
         return product
     } 
+
+    getProductById = async (id) => {
+        const products = await this.getProducts()
+        const result = products.filter((prod) => prod.id == id)
+        if(result.length == 0){
+            console.log(`no se encontro el producto con id ${id}`)
+            return;
+        }
+        return result
+    }
+
+    deleteProductById = async (id) => {
+        const products = await this.getProducts()
+        let filterProduct = []
+        if(products.length > 0){
+            filterProduct = products.filter((prod) => prod.id !== id)
+        }
+
+        if(filterProduct.length === products.length-1){
+            await fs.promises.writeFile(path, JSON.stringify(filterProduct, null, "\t"))
+            console.log(`El producto con id: ${id} fue eliminado.`);
+        } else {
+            console.log(`No existe producto con id: ${id}.`);
+        }
+    }
+    deleteProductByCode = async (code) => {
+        const products = await this.getProducts()
+        let filterProduct = []
+        if(products.length > 0){
+            filterProduct = products.filter((prod) => prod.code !== code)
+        }
+
+        if(filterProduct.length === products.length-1){
+            await fs.promises.writeFile(path, JSON.stringify(filterProduct, null, "\t"))
+            console.log(`El producto con code: ${code} fue eliminado.`);
+        } else {
+            console.log(`No existe producto con code: ${code}.`);
+        }
+    }
+    updateProduct = async (id, productUpdate) => {
+        const products = await this.getProducts()
+        if(products.length > 0){
+            const productIdx = products.findIndex((p)=> p.id === id)
+            if(productIdx === -1){
+                console.log("no existe el producto con indice " + id)
+                return;
+            }
+            if(products[productIdx].code === productUpdate.code){
+                console.log(`Existe un producto con code: ${productUpdate.code}. No se actualiza el producto.`)
+                return;
+            }
+            // el producto buscado existe, no hay problemas con el code, entonces lo actualizo
+            products[productIdx] = {...products[productIdx], ...productUpdate};
+            await fs.promises.writeFile(path, JSON.stringify(products, null, "\t"))
+            // si se actualizo, logueamos por consola
+            console.log(`Producto con id: ${id} actualizado.`);
+        }else {
+            // no hay productos
+            console.log("No hay productos. No se puede realizar la actualización.");
+          }
+    }
 }
 
 const products = new ProductsManager()
 
 const env = async () => {
-    console.log(await products.getProducts())
+    //console.log(await products.getProducts())
     //console.log(await products.createProducts("termica", "220v", 1500, "terminca.jpg", "abc129", 15))
     //console.log(await products.createProducts("termica", "220v", 1500, "terminca.jpg", "abc125", 15))
     //console.log(await products.createProducts("termica", "220v", 1500, "terminca.jpg", "abc126", 15))
@@ -72,6 +133,10 @@ const env = async () => {
     //console.log(await products.createProducts("termica", "220v", 1500, "terminca.jpg", "abc128", 15))
     //console.log(await products.getProducts())
     //console.log(await products.getProductByCode("abc125"))
+    //console.log(await products.getProductById(4))
+    //console.log(await products.deleteProductById(4))
+    //console.log(await products.deleteProductByCode("abc126"))
+    console.log(await products.updateProduct(2, {code: "abc131"}))
 }
 
 env()
